0a9c0c4f7dc9d85a0ca6a863a11074fd7cf25a17
Priyankasmonde <144534727+Priyankasmonde@users.noreply.github.com>
2026-08-01 20:40:35 +0530

    Delete hre directory

--- hre/src/main/java/com/hotel/hre/entity/ReservationStatus.java
+++
@@ -1,8 +0,0 @@
-package com.hotel.hre.entity;
-
-public enum ReservationStatus {
-    CONFIRMED,
-    CANCELLED,
-    MAINTENANCE_BLOCK,   // used by admins to block a room for maintenance
-    COMPLETED
-}
--- hre/src/main/java/com/hotel/hre/entity/RoomType.java
+++
@@ -1,9 +0,0 @@
-
-package com.hotel.hre.entity;
-
-public enum RoomType {
-    STANDARD,
-    DELUXE,
-    SUITE,
-    FAMILY
-}
